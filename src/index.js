282850bf75fb7083b42af98a51715fb55f1176e7
Najma Abdi <anajmabdi@gmail.com>
2022-10-29 18:03:39 -0700

    add business logic for exchanging currency

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ConvertService from './convert.js';
+
+// business logic
+
+function exchange(targetCurr, amountUSD) {
+  ConvertService.exchange(targetCurr, amountUSD).then(function (response) {
+    if (response.conversion_rate) {
+      printElements(response, targetCurr, amountUSD);
+    } else {
+      printError(response, targetCurr);
+    }
+  });
+}
+
+// UI logic
